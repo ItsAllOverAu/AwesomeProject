7359fcb7148f052ac633e5ecc37f978fee5026b9
Mark <markj.andrews@gmail.com>
2019-01-09 16:04:46 +1100

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/ItsAllOverAu/AwesomeProject/util"
+
+func main() {
+	println("Hello World!")
+
+	println(util.Sum(1, 2))
+}
---
+++ util/util.go
@@ -0,0 +1,7 @@
+package util
+
+func Sum(val1 int, val2 int) (result int) {
+	result = val1 + val2
+
+	return result
+}
